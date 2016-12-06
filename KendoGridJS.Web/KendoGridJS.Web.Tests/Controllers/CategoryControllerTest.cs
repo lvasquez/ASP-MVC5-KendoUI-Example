4b11ab44aa98d89941658d55a85b2c919e56c115
Luis Vasquez <levb20@gmail.com>
2016-12-05 23:24:08 -0600

    Add CategoryController Unit Tests
    Add basic unit tests for Category Controller

--- KendoGridJS.Web/KendoGridJS.Web.Tests/Controllers/CategoryControllerTest.cs
+++ KendoGridJS.Web/KendoGridJS.Web.Tests/Controllers/CategoryControllerTest.cs
@@ -2,6 +2,10 @@
 using System.Text;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KendoGridJS.Web.Controllers;
+using System.Web.Mvc;
+using KendoGridJS.Web.Models;
+using System.Linq;
 
 namespace KendoGridJS.Web.Tests.Controllers
 {
@@ -11,13 +15,80 @@ namespace KendoGridJS.Web.Tests.Controllers
     [TestClass]
     public class CategoryControllerTest
     {
-       
+
         [TestMethod]
-        public void TestMethod1()
+        public void Index_Return_NotNull()
         {
-            //
-            // TODO: Add test logic here
-            //
+            var controller = new CategoryController();
+
+            var result = controller.Index();
+            var rresult = (ActionResult)result;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(rresult, typeof(ActionResult));
+        }
+
+
+        [TestMethod]
+        public void Read_Return_NotNull()
+        {
+            var controller = new CategoryController();
+
+            var result = controller.Read();
+            var rresult = (JsonResult)result;
+            var listResult = (IEnumerable<CategoryViewModel>)rresult.Data;
+
+            Assert.IsInstanceOfType(rresult.Data, typeof(IList<CategoryViewModel>));
+            Assert.IsNotNull(result);
+            Assert.AreEqual(listResult.Count(), 2);
+        }
+
+
+        [TestMethod]
+        public void Create_Return_NotNull()
+        {
+            var controller = new CategoryController();
+
+            IList<CategoryViewModel> list = new List<CategoryViewModel>();
+
+            var result = controller.Create(list);
+            var rresult = (JsonResult)result;
+            var listResult = (IEnumerable<CategoryViewModel>)rresult.Data;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(rresult.Data, typeof(List<CategoryViewModel>));
+            Assert.AreEqual(listResult.Count(), 2);
+        }
+
+        [TestMethod]
+        public void Update_Return_NotNull()
+        {
+            var controller = new CategoryController();
+
+            IList<CategoryViewModel> list = new List<CategoryViewModel>();
+
+            var result = controller.Update(list);
+            var rresult = (JsonResult)result;
+
+            Assert.IsNull(rresult.Data);
+            Assert.IsNotInstanceOfType(rresult.Data, typeof(List<CategoryViewModel>));
+
+        }
+
+
+        [TestMethod]
+        public void Delete_Return_NotNull()
+        {
+            var controller = new CategoryController();
+
+            IList<CategoryViewModel> list = new List<CategoryViewModel>();
+
+            var result = controller.Delete(list);
+            var rresult = (JsonResult)result;
+
+            Assert.IsNull(rresult.Data);
+            Assert.IsNotInstanceOfType(rresult.Data, typeof(List<CategoryViewModel>));
+
         }
     }
 }
---
+++ KendoGridJS.Web/KendoGridJS.Web/Controllers/CategoryController.cs
@@ -0,0 +1,60 @@
+﻿using KendoGridJS.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KendoGridJS.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Read()
+        {
+
+            IList<CategoryViewModel> list = new List<CategoryViewModel>()
+            {
+                new CategoryViewModel() { CategoryID = 1, CategoryName = "Fruits", Description = "Only Fruits" },
+                new CategoryViewModel() { CategoryID = 2, CategoryName = "No Fruits", Description = "Only No Fruits" }
+            };
+
+            return Json(list.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Create(IEnumerable<CategoryViewModel> categories)
+        {
+            IList<CategoryViewModel> list = new List<CategoryViewModel>()
+            {
+                new CategoryViewModel() { CategoryID = 1, CategoryName = "Fruits", Description = "Only Fruits" },
+                new CategoryViewModel() { CategoryID = 2, CategoryName = "No Fruits", Description = "Only No Fruits" }
+            };
+
+
+            return Json(list.ToList());
+        }
+
+
+        [HttpPost]
+        public ActionResult Update(IEnumerable<CategoryViewModel> categories)
+        {
+
+            return Json(null);
+        }
+
+
+        [HttpPost]
+        public ActionResult Delete(IEnumerable<CategoryViewModel> categories)
+        {
+
+            return Json(null);
+        }
+    }
+}
